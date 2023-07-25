4412d6af9745a8a4f42895ca32047bed95e85648
Martin Nyaga <martin.nyaga@student.moringaschool.com>
2023-07-25 13:37:13 +0300

    Completed assignment

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,11 +1,31 @@
 import React, { useState } from "react";
 
-function ItemForm() {
+function ItemForm({addedItem}) {
   const [name, setName] = useState("");
   const [category, setCategory] = useState("Produce");
 
+  const createShoppingList = (e) => {
+    e.preventDefault();
+
+    const newShopping = {
+      name: name,
+      category: category,
+      isInCart: false,
+    }
+
+    fetch('http://localhost:4000/items', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newShopping),
+    })
+      .then((response) => response.json())
+      .then((data) => addedItem(data))
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={createShoppingList}>
       <label>
         Name:
         <input
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
@@ -11,22 +11,55 @@ function ShoppingList() {
     setSelectedCategory(category);
   }
 
+  function handleUpdateItem(updatedItem){
+    const updatedItems = items.map((item) => {
+      if (item.id === updatedItem.id) {
+        return updatedItem;
+      } else {
+        return item;
+      }
+    });
+    setItems(updatedItems);
+  }
+
+  function handleDeletedItems(deleteItem){
+    const updatedItems = items.filter((item) => item.id !== deleteItem.id);
+  setItems(updatedItems);
+  }
+
   const itemsToDisplay = items.filter((item) => {
     if (selectedCategory === "All") return true;
 
     return item.category === selectedCategory;
   });
 
+  const fetchShoppingList = () => {
+    fetch("http://localhost:4000/items") 
+    .then((response) => response.json())
+    .then((data) => setItems(data))
+  }
+
+  useEffect(()=>{
+    fetchShoppingList()
+  }, [])
+
+
+
+  const newItem = (item) => {
+    console.log(item);
+
+    setItems([...items, item]);
+  }
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      <ItemForm addedItem={newItem} />
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
       />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item key={item.id} item={item} onUpdateItem = {handleUpdateItem} onDeleteItem = {handleDeletedItems} />
         ))}
       </ul>
     </div>
